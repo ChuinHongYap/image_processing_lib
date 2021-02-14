ea7064a41ee1c6d9e0cb7dab80b876d5bb038498
ChuinHongYap <57805747+ChuinHongYap@users.noreply.github.com>
2021-02-14 16:47:25 +0800

    Update image size

--- imageAugmentation/image_augmentation.py
+++ imageAugmentation/image_augmentation.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import PIL
 from PIL import Image, ImageEnhance
 
-SAVE_IMAGES = False
 
 img = Image.open('osaka_castle.jpg')
 
@@ -23,32 +22,29 @@ img_contrastMore = img_en.enhance(1.5)
 img_rotate1 = img.rotate(-10, resample=0, expand=0, center=None, translate=None, fillcolor=None)
 img_rotate2 = img.rotate(10, resample=0, expand=0, center=None, translate=None, fillcolor=None)
 
+plt.figure(figsize=(8,4))
+
 # plot images
-plt.subplot(241),plt.imshow(img)
+plt.subplot(2,4,1),plt.imshow(img)
 plt.title('Original', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(242),plt.imshow(img_gray, cmap="gray")
+plt.subplot(2,4,2),plt.imshow(img_gray, cmap="gray")
 plt.title('Grayscale', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(243),plt.imshow(img_hflip)
+plt.subplot(2,4,3),plt.imshow(img_hflip)
 plt.title('Flip Left-Right', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(244),plt.imshow(img_vflip)
+plt.subplot(2,4,4),plt.imshow(img_vflip)
 plt.title('Flip Up-Down', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(245),plt.imshow(img_contrastLess)
+plt.subplot(2,4,5),plt.imshow(img_contrastLess)
 plt.title('Contrast Down', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(246),plt.imshow(img_contrastMore)
+plt.subplot(2,4,6),plt.imshow(img_contrastMore)
 plt.title('Contrast Up', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(247),plt.imshow(img_rotate1)
+plt.subplot(2,4,7),plt.imshow(img_rotate1)
 plt.title('Rotate CW', fontsize=10), plt.xticks([]), plt.yticks([])
 
-plt.subplot(248),plt.imshow(img_rotate2)
+plt.subplot(2,4,8),plt.imshow(img_rotate2)
 plt.title('Rotate C-CW', fontsize=10), plt.xticks([]), plt.yticks([])
-
-# save images for comparison
-if SAVE_IMAGES:
-    plt.savefig('imageAugmentation_comparison.png', dpi=200)
-
